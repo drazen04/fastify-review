217f46b780a76dad3d740eaeaacd224a07206957
drazen04 <hangtime23@hotmail.it>
2023-11-10 11:08:10 +0100

    chore: enrich fp plugin example

--- overview/fastify-plugin/fp-myplugin-index.cjs
+++ overview/fastify-plugin/fp-myplugin-index.cjs
@@ -3,10 +3,16 @@ const myPlugin = require('./fp-myplugin.cjs')
 
 const app = Fastify({ logger: { transport: { target: 'pino-pretty' } } })
 
-app.register(myPlugin)
+// app.register(myPlugin)
 app.decorate('root', 'This is (G)root')
+async function newPlugin(app) {
+    app.decorate('newPlugin', 'Hi from newPlugin')
+}
+// newPlugin[Symbol.for('skip-override')] = true
+app.register(newPlugin)
 
 app.ready().then(() => {
     console.log('root -- ', app.root)
-    console.log('root -- ', app.myPlugin)
+    // console.log('root -- ', app.myPlugin)
+    console.log('root -- ', app.newPlugin)
 })
