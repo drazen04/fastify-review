0e4d92b43d42df71bbb71454d8df93b00774041d
drazen04 <hangtime23@hotmail.it>
2023-11-10 16:22:19 +0100

    chore: add inner plugin + printPlugins

--- overview/plugin-context.cjs
+++ overview/plugin-context.cjs
@@ -7,17 +7,21 @@ const app = Fastify({logger: { transport: {
 async function run () {
     
     app.register(function firstP (app, opts, next) {
+        
+        app.register(async function innerPlugin (app, opts) {
+
+        })
         next()
     })
-    app.register(async function secondP (app) {
+    app.register(function secondP (app, opts, next) {
+        next()
     })
     app.register(function thirdP (app, opts, next) {
         next()
     })
-    app.get('/', async (req, res) => 'ciao')
 
     await app.listen({
         port: 8080
-    }).then(() => app.log.info('Server Started'))
+    }).then(() => console.log(app.printPlugins()))
 }
 run()
