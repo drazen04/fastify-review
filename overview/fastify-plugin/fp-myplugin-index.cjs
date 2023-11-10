39af6a59ee3483b0d8ece33cbcdc341878e5d82a
drazen04 <hangtime23@hotmail.it>
2023-11-10 16:13:54 +0100

    chore: added comments to makes code works

--- overview/fastify-plugin/fp-myplugin-index.cjs
+++ overview/fastify-plugin/fp-myplugin-index.cjs
@@ -3,16 +3,19 @@ const myPlugin = require('./fp-myplugin.cjs')
 
 const app = Fastify({ logger: { transport: { target: 'pino-pretty' } } })
 
+// Remove comment in line below to register plugin with fastify/plugin
 // app.register(myPlugin)
 app.decorate('root', 'This is (G)root')
 async function newPlugin(app) {
     app.decorate('newPlugin', 'Hi from newPlugin')
 }
+// Remove comment in line below to unlock plugin context to parent
 // newPlugin[Symbol.for('skip-override')] = true
 app.register(newPlugin)
 
 app.ready().then(() => {
     console.log('root -- ', app.root)
+    // Remove comment in line below to see result of myPlugin
     // console.log('root -- ', app.myPlugin)
     console.log('root -- ', app.newPlugin)
 })
